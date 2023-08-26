9352fa48f81cde75c0e7dc50a6370ffcde1d88cf
Blaine Kennedy <blainekennedy@gmail.com>
2023-08-25 21:46:19 -0500

    refactor count and to string

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -16,17 +16,17 @@ class LinkedList
     data
   end
   def count
-    @current_node = @head
+    current_node = @head
     count = 0
     
-    while @current_node 
+    while current_node 
       count += 1
-      @current_node = @current_node.next_node
+      current_node = current_node.next_node
     end
     count
   end
   def to_string
-    @current_node = @head
-    @current_node.data.to_s
+    current_node = @head
+    current_node.data.to_s
   end
 end
