de2e48b356931644ed807f59e2b95593e917fbaf
bkchilidawg <138528386+bkchilidawg@users.noreply.github.com>
2023-08-28 16:52:46 -0500

    Feature/add iteration 3 (#2)
    * feat: add count and append to beat_box
    
    * test: add test for #say
    
    * feat: Add play method
    
    * feat: Change voice

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,10 +1,26 @@
 class BeatBox
-  attr_reader :LinkedList
+  attr_reader :list
   def initialize
-  @LinkedList = LinkedList
+  @list = LinkedList.new
+  end
+  def append(data)
+    splits = data.split(" ")
+    splits.select do |split|
+      @list.append(split)
+    end
+  end
+  def next_node
+    @list.current_node.next_node
+  end
+  def count 
+    @list.count
+  end
+  def play
+    beats = @list.to_string
+    beats = beats.split(" ")
+    beats.select do |beat|
+      `say -r 500 -v Cellos #{beat}`
+    end
   end
 
-  def list
-    @list
-  end
 end
